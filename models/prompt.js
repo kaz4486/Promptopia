4306d0c20bc3965627e44c262c5184105b890b1d
Kamil <kaz4486@gmail.com>
2023-09-07 20:23:46 +0200

    Add create-post page, Add prompt Schema, Add POST route for new prompt

---
+++ models/prompt.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema, models, model } from 'mongoose';
+
+const PromtSchema = new Schema({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  prompt: {
+    type: String,
+    required: [true, 'Prompt is required.'],
+  },
+  tag: {
+    type: String,
+    required: [true, 'Tag is required.'],
+  },
+});
+
+const Prompt = models.Prompt || model('Prompt', PromtSchema);
+
+module.exports = Prompt;
