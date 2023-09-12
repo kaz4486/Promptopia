cc0f44c0e7dd7aa731d3f15919afb86404d383cf
Kamil <kaz4486@gmail.com>
2023-09-12 20:21:12 +0200

    Add route for user/profile

--- app/api/users/[id]/profile/route.js
+++ app/api/users/[id]/profile/route.js
@@ -1,15 +1,15 @@
 import { connectToDB } from '@utils/database';
-import Prompt from '@models/prompt';
+import User from '@models/user';
 
 export const GET = async (request, { params }) => {
   try {
     await connectToDB();
 
-    const prompts = await Prompt.find({ creator: params.id }).populate(
-      'creator'
-    );
+    const user = await User.findById(params.id).populate('creator');
 
-    return new Response(JSON.stringify(prompts), {
+    if (!user) return Response('User not found', { status: 404 });
+
+    return new Response(JSON.stringify(user), {
       status: 200,
     });
   } catch (error) {
